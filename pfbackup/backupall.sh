0fa4ba5fcec583e754718866a74f47f42c5e7433
andretapxure <49208838+andretapxure@users.noreply.github.com>
2020-09-04 15:51:25 -0300

    Update backupall.sh

--- pfbackup/backupall.sh
+++ pfbackup/backupall.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+#This script reads a simple text file (in this case, hosts) and gets the hosts info to call the pfbackup.sh script and make the backup of each firewall
 while IFS=, read -r host user password desc
 do
     echo "Backing up $host"
